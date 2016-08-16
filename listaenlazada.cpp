e0da6f7e9b58fa2223db2099645ab1a083c35826
Rafastaria <guitronleafar@gmail.com>
2016-08-16 15:31:24 -0500

    second commit

--- listaenlazada.cpp
+++ listaenlazada.cpp
@@ -1,32 +1,42 @@
-#include <stdio.h>
+	#include <stdio.h>
 
-typedef struct NodoLista {
+typedef struct NodoContacto {
 
-char *Contacto;
-struct NodoLista *sig;
+char Nombre[100];
+unsigned edad;
+float celular;
+char carrera[100];
 
-}Nodo;
+int contador;
+NodoContacto *sig;
 
+}NodoContacto;
 
-typedef struct Listas {
-
-Nodo *inicio;
-Nodo *fin;
-	
-}Lista;
-
-
-void crearlista(Lista *lista) {
-
-int tam;
-
-	tam = 0;
-	lista->inicio = NULL;
-	lista->fin = NULL;
-		
-}
 
 int main(){
 
+
+
+struct NodoContacto Cuenta1={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta2={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta3={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta4={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta5={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta6={"Rafael", 20, 9982607763, "Telematica"};
+struct NodoContacto Cuenta7={"Rafael", 20, 9982607763, "Telematica"};
+
+
+Cuenta1.sig=&Cuenta2;
+Cuenta2.sig=&Cuenta3;
+Cuenta3.sig=&Cuenta4;
+Cuenta4.sig=&Cuenta5;
+Cuenta5.sig=&Cuenta6;
+Cuenta6.sig=&Cuenta7;
+Cuenta7.sig= NULL;
+
+
+
 return 0;
 }
+
+
