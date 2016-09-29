fbd16332aa38ec14460144342d5368ed71e4d9c4
Rafastaria <guitronleafar@gmail.com>
2016-09-28 19:14:57 -0500

    listas dobles

--- listasdobles.cpp
+++ listasdobles.cpp
@@ -18,24 +18,28 @@ void recorrer_derecha(NodoContacto *, int num);
 struct NodoContacto *prim, *ult;
 
 int main(){
-int a, opc, num;
+int a, opc, num, pos;
 prim = (struct NodoContacto *) NULL;
 ult = (struct NodoContacto *) NULL;
     do {
-        printf("\nEliga su opcion: \n1.Crear lista doble\n2. Recorrer lista de derecha a izquierda \n3.Recorrer lista izquierda a derecha \n4. Salir\n");
+        printf("\nElija su opcion: \n1.Crear lista doble \n2.Insertar antes de n \n3. Recorrer lista de derecha a izquierda \n4.Recorrer lista izquierda a derecha \n5. Salir\n");
         scanf("%d",&opc);
         switch(opc){
             
             case 1: printf("\nCuantos contactos=");
-            scanf("%i",&num);
+            scanf("%d",&num);
             agregar_contacto(num);break;
-            case 2: recorrer_izquierda(prim);break;
-            case 3: recorrer_derecha(ult, num); break;
-            case 4: break;
+            case 2: printf("\nPosicion:");
+                    scanf("%d",&pos);
+                    pos= pos-1;
+                    agregar_contacto_antes_de_x(pos);break;
+            case 3: recorrer_izquierda(prim);break;
+            case 4: recorrer_derecha(ult, num); break;
+            case 5: break;
             default: printf("Operacion no valida\n");
             break;
         }
-    }while(opc!=4);
+    }while(opc!=5);
 return 0;
 }
 
@@ -43,7 +47,7 @@ void agregar_contacto(int TAM){
 
 struct NodoContacto *nuevo;
     if (nuevo==NULL){
-    printf("Agregado por adelante\n");
+    printf("Lista Creada\n");
     }
     for(int i=1;i<=TAM;i++){
         nuevo = (struct NodoContacto *) malloc (sizeof (struct NodoContacto));
@@ -89,19 +93,37 @@ void recorrer_derecha(NodoContacto[], int num){
     printf("\nEsta es la lista: \n");
     while (aux!=NULL){
         printf("Nombre: %s \n num: %d\n Celular: %s\nCarrera: %s\n\n", aux->Nombre, aux->num, aux->celular, aux->carrera);
-    aux=aux->ant;
-    con--;
+        aux=aux->ant;
+        con--;
     }
     if (con==0) {
         printf("\n");
     }
 }
 
-void agregar_contacto_antes_de_x(int){
+void agregar_contacto_antes_de_x(int POS){
+    int i=0;
+    struct NodoContacto *nuevo;
+    struct NodoContacto *actual;
+    nuevo = (struct NodoContacto *) malloc (sizeof (struct NodoContacto));
+    strcpy(nuevo->Nombre, "Rafa");
+    nuevo->num=1;
+    strcpy(nuevo->celular, "9982145027");
+    strcpy(nuevo->carrera, "Telematica");
+    actual=prim;
 
+    while(actual->num!=POS && actual->sig != NULL){
+        actual=actual->sig;
 
-
-
-
-
+    }
+    nuevo->sig = actual;
+    nuevo->ant = actual->ant;
+    if(actual->ant == NULL){
+        prim=nuevo;
+    }
+    else{
+        actual->ant->sig=nuevo;
+    }
+    actual->ant=nuevo;
+    
 }
